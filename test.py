66047d39cc23ae50178602020c3a80c79d0d4bc0
spline <reticulatingspline@github.com>
2014-10-12 06:29:52 -0400

    Add in tests.

--- test.py
+++ test.py
@@ -9,6 +9,12 @@ from supybot.test import *
 
 class OddsTestCase(PluginTestCase):
     plugins = ('Odds',)
+    
+    def testOdds(self):
+        self.assertNotError('odds nhl')
+        self.assertNotError('odds nfl')
+        self.assertNotError('odds mlb')
+    
 
 
 # vim:set shiftwidth=4 tabstop=4 expandtab textwidth=79:
